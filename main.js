a1429763cf0ce7573d3446dff33daa189c037cd9
Phoomtep <Phoombnw@gmail.com>
2024-04-14 23:07:46 +0700

    +fade animation

--- main.js
+++ main.js
@@ -10,3 +10,17 @@ navColumn.addEventListener('click', () => {
         menuText.innerText = "menu";
     }
 });
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el))
